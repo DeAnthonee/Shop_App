cad9c04ef9ca7344b7c9efb6a7d3ba49b815f72f
DeAnthonee <33053257+DeAnthonee@users.noreply.github.com>
2020-04-26 14:56:48 -0500

    Added product object and skeleton of details page (#1)
    Co-authored-by: DeAnthonee Greenfield <espiiking@gmail.com>

--- app/src/main/java/com/deanthonee/shopapp/screens/details/DetailsActivity.kt
+++ app/src/main/java/com/deanthonee/shopapp/screens/details/DetailsActivity.kt
@@ -2,18 +2,34 @@ package com.deanthonee.shopapp.screens.details
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.deanthonee.shopapp.R
+import com.deanthonee.shopapp.models.Product
+import kotlinx.android.synthetic.main.activity_details.details_brand_textview
+import kotlinx.android.synthetic.main.activity_details.details_category_textview
+import kotlinx.android.synthetic.main.activity_details.details_price_textview
 
 class DetailsActivity : AppCompatActivity() {
 
     companion object {
+        const val PRODUCT_KEY = "product_key"
 
-        fun newIntent(context: Context) = Intent(context,DetailsActivity::class.java)
+        fun newIntent(context: Context, product: Product): Intent {
+            val intent: Intent = Intent(context, DetailsActivity::class.java)
+            intent.putExtra(PRODUCT_KEY, product)
+            return intent
+        }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+
+        val product = intent.getParcelableExtra<Product>(PRODUCT_KEY)
+        details_brand_textview.text = product?.brand
+        details_category_textview.text = product?.category
+        details_price_textview.text = product?.price
+        title = product.name
     }
 }
