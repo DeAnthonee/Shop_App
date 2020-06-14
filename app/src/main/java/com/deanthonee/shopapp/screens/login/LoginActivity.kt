cbcf2a2c2eb92a2aac623707040ff46fc9926d63
DeAnthonee <33053257+DeAnthonee@users.noreply.github.com>
2020-06-14 13:26:25 -0700

    Deanthonee/login base ui (#2)
    * wip
    
    * Added ui for login screen
    
    Co-authored-by: DeAnthonee Greenfield <d.greenfield@accenture.com>

--- app/src/main/java/com/deanthonee/shopapp/screens/login/LoginActivity.kt
+++ app/src/main/java/com/deanthonee/shopapp/screens/login/LoginActivity.kt
@@ -3,16 +3,19 @@ package com.deanthonee.shopapp.screens.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.deanthonee.shopapp.R
+import com.deanthonee.shopapp.databinding.ActivityLoginBinding
 import com.deanthonee.shopapp.screens.home.HomeActivity
 import kotlinx.android.synthetic.main.activity_login.login_button
 
 class LoginActivity : AppCompatActivity() {
 
+    lateinit var bindings:ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        bindings = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(bindings.root)
 
-        login_button.setOnClickListener {
+        bindings.loginButton.setOnClickListener {
             startActivity(HomeActivity.newIntent(this))
         }
     }
